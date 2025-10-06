62915d9031b577b36046d003cbade9f9bfd1cb96
Pedro Fialho <pedro_fialho1994@hotmail.com>
2025-10-06 13:15:05 +0100

    Added folder for neovim config

---
+++ nvim/ftplugin/java.lua
@@ -0,0 +1,6 @@
+local jdtls_config = {
+    cmd = {'jdtls'},
+    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+}
+
+require('jdtls').start_or_attach(jdtls_config)
---
+++ nvim/lua/plugins/godebug.lua
@@ -0,0 +1,3 @@
+return {
+	'jodosha/vim-godebug'
+}
